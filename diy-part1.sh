dab9b08c925755b75594bf1fc67fa826993a94b3
kingyan <kingyan86@gmail.com>
2020-05-27 15:20:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,11 +5,11 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 # Lisence: MIT
 # Author: KingYan P3TERX
-# Build 2020-02-18 19:15
+# Build 2020-05-27 15:20
 #=============================================================
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
